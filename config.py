e99338acb57607ceb302900aa10d954b623f84bf
William Finneran <finneranbilly@gmail.com>
2019-10-26 14:11:46 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "EeCVMcgF7RmJrOJp7BFXd4yPu"
+consumer_secret = "CMHUX7FSlsS8lbepD7z3OZpw4DfvBu4DchPU38VhOQo8NtObDT"
+access_token = "1134261409527795712-R1APD5pptCht0XYrCuYfUEMzZhHXwH"
+access_token_secret = "naaqkSq5ZqBq8LyLGinHnd0D8b2KA6N6lP5njBdbgtmNX"
